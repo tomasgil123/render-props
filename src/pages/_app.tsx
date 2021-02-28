513d0e36a6abaae5fc87a0f33bcfae072488c730
Tomas Gil <tomasgil1234@gmail.com>
2021-02-28 18:50:41 -0300

    feat: add tailwind

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
 import 'src/styles/globals.css'
+import 'tailwindcss/tailwind.css'
 import PageWithLayoutType from 'src/types/pageWithLayout'
 import React from 'react'
+import { QueryClient, QueryClientProvider } from 'react-query'
+const queryClient = new QueryClient()
 
 type AppLayoutProps = {
   Component: PageWithLayoutType
@@ -12,9 +15,11 @@ function MyApp({ Component, pageProps }: AppLayoutProps): JSX.Element {
   const Layout = Component.layout ? Component.layout : React.Fragment
 
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <QueryClientProvider client={queryClient}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </QueryClientProvider>
   )
 }
 
